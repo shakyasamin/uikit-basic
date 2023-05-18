1d8a1f03c20a863b252b3264d6a599a4cbac6fb8
saminshakya <saminshakya27@gmail.com>
2023-05-18 09:57:46 +0545

    storyboard basic

---
+++ UIKit-Intro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Intro
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 17/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
